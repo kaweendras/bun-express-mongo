f7e611d34964a05479fc55376829d6b134845c57
kaweendras <salitha.wijerathna@gmail.com>
2025-03-18 15:09:12 +0530

    CHG | Enhance MongoDB connection logging and remove forced exit on error

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -2,11 +2,17 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    process.stdout.write("📦 Connecting to MongoDB");
+    const interval = setInterval(() => {
+      process.stdout.write(".");
+    }, 1000);
     await mongoose.connect(process.env.MONGO_URI!);
-    console.log("📦 MongoDB Connected");
+    process.stdout.write("\n");
+    clearInterval(interval);
+    console.log("✅MongoDB Connected");
   } catch (error) {
     console.error("❌ MongoDB Connection Error", error);
-    process.exit(1);
+    // process.exit(1);
   }
 };
 
