9c3c58d9e1f255b3b49dc324acaa815808421f2a
kaweendras <salitha.wijerathna@gmail.com>
2025-03-18 15:09:19 +0530

    CHG | Initialize database connection on server start

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { createServer } from "http";
 import app from "./app";
+import connectDB from "./config/db.config";
 
 const PORT = process.env.PORT || 3000;
 const server = createServer(app);
 
-server.listen(PORT, () => {
+server.listen(PORT, async () => {
+  await connectDB();
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
